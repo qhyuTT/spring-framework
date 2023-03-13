b6093ea341420c46ef35c4fc715a46ebb34a6e27
yuqihai <yuqihai@travelsky.com>
2023-03-13 11:47:13 +0800

    lettcode算法题 最长回文

--- spring-qhyu/src/main/java/com/qhyu/cloud/leetcode/LongestPalindrome.java
+++ spring-qhyu/src/main/java/com/qhyu/cloud/leetcode/LongestPalindrome.java
@@ -91,43 +91,50 @@ public class LongestPalindrome {
 
 	public static Integer way4(String str, int index) {
 		char[] chars = str.toCharArray();
-		return process4(str.toCharArray(), 0,index, chars.length - 1);
-
+		int[] continuous = new int[chars.length];
+		return process4(str.toCharArray(), index, chars.length - 1, continuous);
 	}
 
-	// 我需要找到所有的basecase
-	//
-	private static Integer process4(char[] chars, int max, int left, int right) {
-		StringBuffer str5 = new StringBuffer();
-		for (char str : chars) {
-			str5.append(str);
-		}
-		System.out.println(str5);
-		if (chars.length == 0) {
-			return max + 0;
-		} else if (left == right) {
-			return max + 1;
+	private static Integer process4(char[] chars, int left, int right, int[] continuous) {
+		if (left == right) {
+			return 1;
 		} else if (left + 1 == right) {
-			return max + 2;
+			if (chars[left] == chars[right]) {
+				return 2;
+			}
+			return Integer.MIN_VALUE;
 		} else {
 			int p1 = 0;
-			String s1 = str5.substring(1, chars.length - 1);
-			if (process4(s1.toCharArray(), max, 0, s1.length() - 1)>0){
-				p1 = max+2;
-			}else{
-				return Integer.MIN_VALUE;
+			if (chars[left] == chars[right]) {
+				if (left == 0 || continuous[left - 1] == 1) {
+					continuous[left] = 1;
+					p1 = process4(chars, left + 1, right - 1, continuous) + 2;
+				} else {
+					return Integer.MIN_VALUE;
+				}
 			}
-			// 如果不相等，那么整理下这个chars，重新传
-			String s2 = str5.substring(1, chars.length);
-			int p2 = process4(s2.toCharArray(), 0, 0, s2.length() - 1);
 
-			String s3 = str5.substring(0, chars.length - 1);
-			int p3 = process4(s3.toCharArray(), 0, 0, s3.length() - 1);
-
+			int[] copy2 = new int[chars.length];
+			copy2[left] =1;
+			int[] copy3 = new int[chars.length];
+			if (left != 0){
+				copy3[left-1] = 1;
+			}
+			int p2 = process4(chars, left + 1, right, copy2);
+			int p3 = process4(chars, left, right - 1, copy3);
+			// 遍历int，把是1的拿出来，再从chars中去拿对应的下标就完事了
 			return Math.max(p1, Math.max(p2, p3));
 		}
 	}
 
+	public static void main(String[] args) {
+		System.out.println(way4("eeeaaa", 0));
+		System.out.println(way4("xeeee", 0));
+		System.out.println(way4("eeeex", 0));
+		System.out.println(way4("xxweeeeeweeeee", 0));
+		System.out.println(way4("baba", 0));
+		System.out.println(way4("babab", 0));
+	}
 
 	public static String way2(String str, int index) {
 		char[] chars = str.toCharArray();
@@ -135,11 +142,6 @@ public class LongestPalindrome {
 
 	}
 
-	public static void main(String[] args) {
-		//System.out.println(way3("er12tewrteeeeeeeeeeenmhjyuk21yu", 0));
-		System.out.println(way4("eweeeeweeeee", 0));
-		//System.out.println(way2("babad", 0));
-	}
 
 	private static StringBuilder process2(char[] chars, StringBuilder result, int left, int right) {
 		if (chars.length <= 1 || left < 0 || right > Integer.MAX_VALUE) {
